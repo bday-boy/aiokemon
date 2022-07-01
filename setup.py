ef6f0c3e75948552b8b56a1fbf01caf372192988
bday-boy <asmith4642@gmail.com>
2022-06-30 20:22:27 -0700

    Created initial setup.py file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+def readme() -> str:
+    with open('./README.md') as f:
+        return f.read()
+
+
+setup(
+    name='aiokemon',
+    packages=find_packages(),
+    version='1.0',
+    description='An asynchronous Python wrapper for making PokéAPI requests',
+    long_description=readme(),
+    author='Birthday Boy',
+    author_email='setup.pymademedothis@gmail.com',
+    url='https://github.com/bday-boy/aiokemon',
+    keywords=['pokemon', 'wrapper', 'RESTAPI'],
+    install_requires=['aiohttp-client-cache', 'aiosqlite', 'requests'],
+    license='MIT License'
+)
